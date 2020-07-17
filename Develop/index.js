9dc80f9423f0a4480892a47b145b82f1360014ec
Afzal Akhtar <afzalakhtar@Afzals-Air.lan>
2020-07-16 19:50:10 -0700

    readme q's

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,70 @@
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
+
+        {   type: 'input',
+            name: 'repoName',
+            message: 'What is the name of this project?'
+
+        },
+
+        {   type: 'input'
+            name: 'gitHubName'
+            message: 'What is your Github username?'
+        
+        }
+    
+        {   type: 'input',
+            message: 'Write a description of this application',
+            name: 'description',
+         
+        },
+
+        {   type: 'input',
+            message: 'Write a table of contents for this application',
+            name: 'description',
+         
+        },
+        
+        {   type: 'input',
+            message: 'How do you install your application?',
+            name: 'installApp',
+         
+        },
+       
+        {   type: 'input',
+            message: 'How does a user use your application?',
+            name: 'appInstructions',
+         
+        },
+        
+        {   type: 'input',
+            message: 'What license they would you  like to use? (ex: MIT) Write "none" if you do not wish to use a license type ',
+            name: 'license',
+         
+        },
+
+        {   type: 'input',
+            message: 'Who else contributed to your application? Write "none" if no one contributed ',
+            name: 'contributors',
+         
+        },
+
+        {   type: 'input',
+            message: 'What questions do you have? Write "none" if no tests are needed ',
+            name: 'tests',
+         
+        },
+
+        {   type: 'input',
+            message: 'What tests will you run? Write "none" if no questions ',
+            name: 'questions',
+         
+        },
+    
 
 ];
 
